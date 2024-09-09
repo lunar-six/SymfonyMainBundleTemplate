283c8fa40332399295056e2707ef6bf8dde78582
Alexandra Aurora Göttlicher <toluene-lapdogs07@icloud.com>
2024-09-09 11:37:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# SymfonyMainBundleTemplate
+My Symfony main bundle template.
+
+## Usage
+1. Clone this template
+2. Search and replace "Template" with the desired namespace or bundle name
---
+++ src/Kernel.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+
+namespace Template;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
